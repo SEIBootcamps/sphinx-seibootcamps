2a3e9f75e2de93a703c09c2cefa31da9d9301740
Ashley Trinh <itsashley@hey.com>
2023-10-12 17:20:26 -0700

    chore: get rid of js/

--- js/emphasize-lines.js
+++
@@ -1,9 +0,0 @@
-// Help make styling emphasized lines easier. Find all span.hll elements and
-// set their parent's class to `emphasized`.
-export default function emphasizeLines() {
-  document.querySelectorAll("span.hll").forEach((element) => {
-    const parent = element.parentElement;
-    !parent.classList.contains("emphasized") &&
-      parent.classList.add("emphasized");
-  });
-}
